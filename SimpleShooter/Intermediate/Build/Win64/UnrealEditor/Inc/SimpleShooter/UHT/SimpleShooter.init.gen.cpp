250b6419dfb0a6f83dd2aff3616667e2830986bd
Alexis Schwindt <alexis.schwindt@outlook.com>
2023-02-13 16:57:45 -0300

    added movement, camera control and jumping with M&K

--- SimpleShooter/Intermediate/Build/Win64/UnrealEditor/Inc/SimpleShooter/UHT/SimpleShooter.init.gen.cpp
+++ SimpleShooter/Intermediate/Build/Win64/UnrealEditor/Inc/SimpleShooter/UHT/SimpleShooter.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeSimpleShooter_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x5908ADBD,
-				0xF2BEF94C,
+				0xD8F04760,
+				0x03184561,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_SimpleShooter.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_SimpleShooter.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SimpleShooter(Z_Construct_UPackage__Script_SimpleShooter, TEXT("/Script/SimpleShooter"), Z_Registration_Info_UPackage__Script_SimpleShooter, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x5908ADBD, 0xF2BEF94C));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SimpleShooter(Z_Construct_UPackage__Script_SimpleShooter, TEXT("/Script/SimpleShooter"), Z_Registration_Info_UPackage__Script_SimpleShooter, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xD8F04760, 0x03184561));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
